c7eaef03559af23e4cdfad86a0358b9375a1df15
Carlos Ceagah <carlosceagah@gmail.com>
2021-06-22 13:06:39 -0300

    chore: initial commit

---
+++ src/screens/SignIn/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import { Styles } from './style'
+
+const SignIn: React.FC = () => {
+  return (
+    <View style={Styles.container}>
+      <Text> Hello World</Text>
+    </View>
+  );
+}
+
+export default SignIn;
